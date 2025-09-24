07712129e9f99a01ac295b9ebb7fd4e565607499
David Klement <kulisak12lp@gmail.com>
2025-09-24 15:59:21 +0200

    Preview - Add typing annotations

--- src/borg/helpers/datastruct.py
+++ src/borg/helpers/datastruct.py
@@ -1,4 +1,20 @@
+from typing import Callable, Generic, Protocol, TypeVar
+
 from .errors import Error
+
+
+class TBufferProtocol(Protocol):
+    def __add__(self, other): ...
+    def __getitem__(self, index): ...
+    def __len__(self) -> int: ...
+
+
+class TBufferCallableProtocol(TBufferProtocol):
+    def __call__(self): ...
+
+
+TBuffer = TypeVar("TBuffer", bound=TBufferProtocol)
+TBufferCallable = TypeVar("TBufferCallable", bound=TBufferCallableProtocol)
 
 
 class StableDict(dict):
@@ -8,7 +24,7 @@ class StableDict(dict):
         return sorted(super().items())
 
 
-class Buffer:
+class Buffer(Generic[TBuffer]):
     """
     Provides a managed, resizable buffer.
     """
@@ -16,7 +32,7 @@ class Buffer:
     class MemoryLimitExceeded(Error, OSError):
         """Requested buffer size {} is above the limit of {}."""
 
-    def __init__(self, allocator, size=4096, limit=None):
+    def __init__(self, allocator: Callable[[int], TBuffer], size: int = 4096, limit: int | None = None) -> None:
         """
         Initialize the buffer: use allocator(size) call to allocate a buffer.
         Optionally, set the upper <limit> for the buffer size.
@@ -27,10 +43,10 @@ class Buffer:
         self.limit = limit
         self.resize(size, init=True)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.buffer)
 
-    def resize(self, size, init=False):
+    def resize(self, size: int, init=False) -> None:
         """
         resize the buffer - to avoid frequent reallocation, we usually always grow (if needed).
         giving init=True it is possible to first-time initialize or shrink the buffer.
@@ -42,7 +58,7 @@ class Buffer:
         if init or len(self) < size:
             self.buffer = self.allocator(size)
 
-    def get(self, size=None, init=False):
+    def get(self, size: int | None = None, init=False) -> TBuffer:
         """
         return a buffer of at least the requested size (None: any current size).
         init=True can be given to trigger shrinking of the buffer to the given size.
@@ -52,7 +68,7 @@ class Buffer:
         return self.buffer
 
 
-class EfficientCollectionQueue:
+class EfficientCollectionQueue(Generic[TBufferCallable]):
     """
     An efficient FIFO queue that splits received elements into chunks.
     """
@@ -60,18 +76,18 @@ class EfficientCollectionQueue:
     class SizeUnderflow(Error):
         """Could not pop_front first {} elements, collection only has {} elements.."""
 
-    def __init__(self, split_size, member_type):
+    def __init__(self, split_size: int, member_type: TBufferCallable) -> None:
         """
         Initializes empty queue.
         Requires split_size to define maximum chunk size.
         Requires member_type to be type defining what base collection looks like.
         """
-        self.buffers = []
+        self.buffers: list[TBufferCallable] = []
         self.size = 0
         self.split_size = split_size
         self.member_type = member_type
 
-    def peek_front(self):
+    def peek_front(self) -> TBufferCallable:
         """
         Returns first chunk from queue without removing it.
         Returned collection will have between 1 and split_size length.
@@ -82,7 +98,7 @@ class EfficientCollectionQueue:
         buffer = self.buffers[0]
         return buffer
 
-    def pop_front(self, size):
+    def pop_front(self, size: int) -> None:
         """
         Removes first size elements from queue.
         Throws if requested removal size is larger than whole queue.
@@ -100,7 +116,7 @@ class EfficientCollectionQueue:
             size -= to_remove
             self.size -= to_remove
 
-    def push_back(self, data):
+    def push_back(self, data: bytes) -> None:
         """
         Adds data at end of queue.
         Takes care to chunk data into split_size sized elements.
@@ -119,13 +135,13 @@ class EfficientCollectionQueue:
             self.buffers[-1] = buffer
             self.size += to_add
 
-    def __len__(self):
+    def __len__(self) -> int:
         """
         Current queue length for all elements in all chunks.
         """
         return self.size
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
         """
         Returns true if queue isn't empty.
         """
